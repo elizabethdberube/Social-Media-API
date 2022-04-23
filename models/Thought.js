0ec0176e548e2c53c539c8de2a1328e57e0b725f
elizabeth berube <elizabethdberube@gmail.com>
2022-04-22 21:40:46 -0400

    Added models. No idea if they are correct lol.

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,62 @@
 const { Schema, Types } = require('mongoose');
+//const userSchema = require('User')
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+},
+    {
+        toJSON: {
+            getters: true,
+        },
+
+        reactions: [reactionSchema],
+    },
+)
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: true,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+},
+    {
+        toJSON: {
+            getters: true,
+        },
+
+    })
+const reactionData = [
+    { reactionBody: 'yay!' },
+    { reactionBody: 'excited!' },
+    { reactionBody: 'so nice' },
+];
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+Thought.create({ name: 'Reactions', reactions: reactionData }, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+
+module.exports = Thought; 
--- models/User.js
+++ models/User.js
@@ -1,1 +1,54 @@
 const { Schema, Types } = require('mongoose');
+// const reactionSchema = require('Reaction');
+// const thoughtSchema = require('Thought');
+
+// User schema
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            email: {
+                type: String,
+                required: true,
+                match: /.+\@.+\..+/,
+                unique: true,
+            },
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+        ],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+
+)
+
+userSchema.virtual('friendCount', {
+    ref: 'User',
+    localField: 'friends',
+    foreignField: '_id',
+});
+
+const User = model('user', userSchema);
+
+module.exports = User; 
