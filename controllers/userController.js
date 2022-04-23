7cacc01b473ed3bde6af069d7f234b5683763351
elizabeth berube <elizabethdberube@gmail.com>
2022-04-23 17:43:49 -0400

    added  userRoute and userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,87 @@
-const router = require('express').Router();
-const { User } = require('../models');
+const { ObjectId } = require('mongoose').Types;
+const { regexpToText } = require('nodemon/lib/utils');
+const { User, Thought } = require('../models');
 
+
+
+const headCount = async () =>
+    User.aggregate()
+        .count('userCount')
+        .then((numberOfUsers) => numberOfUsers);
+
+module.exports = {
+    // get all users
+    getUsers(req, res) {
+        User.find()
+            .then(async (users) => {
+                const userObj = {
+                    users,
+                    headCount: await headCount(),
+                };
+                return res.json(userObj);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+
+    // get single user
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then(async (user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that id' })
+                    : res.json({ user })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.statue(500).json(err);
+            });
+    },
+
+    // create a new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // update user
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { username: req.params.userId },
+            { email: req.params.userId },
+            { $pull: { username: req.params.userId } },
+            { $pull: { email: req.params.userId } },
+            { new: true }
+        ).then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // delete user
+    deleteUser(req, res) {
+        User.findOneAndRemove({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No such user exists' })
+                    : Thought.findOneAndUpdate(
+                        { thoughtText: req.params.userId },
+                        { $pull: { thoughtText: req.params.userId } },
+                        { new: true }
+                    )
+            ).then((user) =>
+                !user
+                    ? res.status(404).json({
+                        message: 'User deleted',
+                    })
+                    : res.json({ message: 'User deleted' })
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+};
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,39 @@
-const { Schema, Types } = require('mongoose');
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:usersId
+router.route('/:usersId')
+    .get(getSingleUser)
+    .post(createUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
